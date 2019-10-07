e61138ccd0f50b50057699876aa46e85baefe3eb
Nilson Kawasaki <nilkawasaki@msn.com>
2019-10-07 16:01:56 -0300

    Criando IF para trazer apenas as notas Assinadas

--- danfe/src/main/java/principal/principal.java
+++ danfe/src/main/java/principal/principal.java
@@ -15,25 +15,26 @@ public class principal {
 		String sUFAutor ="";         //UF do Autor código IBGE pode homitir
 		String sCNPJ = "08618022000202";   //CNPJ do Cliente
 		//String sCNPJ = "00000000000000";
-		String sultNSU = "000000000007250";  // Ultima NSU vem do Banco.
+		String sultNSU = "000000000007245";  // Ultima NSU vem do Banco.
 		//String sultNSU = "000000000000000";
 		//String schNFe = "41190905607657000135550030008577431008637997";
 		String schNFe = "00000000000000000000000000000000000000000000";
 	
 		
 	retornoXml = SefazXml.setXml(stpAmb, sUFAutor, sCNPJ, sultNSU, schNFe);
-	
-	//System.out.println(Estrutura.getNsu());
-	//System.out.println(Estrutura.getSchema());
-	//System.out.println(Estrutura.getXml());
+	System.out.println("CNPJ: " + sCNPJ);
 	System.out.println("UltNSU: "+ retornoXml.getUltNSU());
 	System.out.println("MaxNSU: "+ retornoXml.getMaxNSU());
 	
-	for (XmlEstrutura retorno : retornoXml.getXmlEstrutura()) {
-		System.out.println("NSU: "+ retorno.getNsu());
-		System.out.println("Shema: "+ retorno.getSchema());
-		System.out.println("XML: "+ retorno.getXml());
-	}
+	
+	//For para varer o objeto retornoXml que contém uma lista de XML
+		for (XmlEstrutura retorno : retornoXml.getXmlEstrutura()) {
+			if(retorno.getSchema().equals("procNFe_v4.00.xsd")) {
+				System.out.println("NSU: "+ retorno.getNsu());
+				System.out.println("Shema: "+ retorno.getSchema());
+				System.out.println("XML: "+ retorno.getXml());
+			}
+		}
 	}
 
 }
